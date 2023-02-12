fecfce0e9c07c68991921a5f78c89d00be57f739
minsu <minsu8012@naver.com>
2023-02-12 18:15:52 +0900

    오류 확인 차 커밋

--- src/Networking/URLConnectionEx3.java
+++ src/Networking/URLConnectionEx3.java
@@ -34,7 +34,7 @@ public class URLConnectionEx3 {
 			BufferedReader br=new BufferedReader(isr);//버퍼를 이용한 효율적인 출력
 			
 			File file=new File(".\\src\\Networking\\test.txt");//해당 url를 저장할 파일 생성
-			if(!file.exists()&&!file.isDirectory()) {//파일 유효성 체크(해당 경로에 파일 없으면 생성
+			if(!file.exists()&&!file.isDirectory()) {//파일 유효성 체크(해당 경로에 파일 없으면 생성)
 				file.createNewFile();//해당 파일 생성
 				System.err.println("파일 생성");
 			}else {
@@ -42,15 +42,17 @@ public class URLConnectionEx3 {
 				
 			}
 			
+			//출력해서 보낼 준비 시작
 			
-			FileOutputStream fr=new FileOutputStream(file);
-			PrintStream pw=new PrintStream(fr);
-			System.setOut(pw);		
+			//PrintStream에서 바꿀 예정임... 그러기 위해서는 OutputStream과 관련된 스트림 생성
+			FileOutputStream fr=new FileOutputStream(file);//출력 스트림 생성(괄호 안에는 출력하고자 하는 File 넣기)
+			PrintStream pw=new PrintStream(fr);//출력 변경 스트림 객체 생성
+			System.setOut(pw);//System.out 호출시 콘솔이 아닌 지정한 파일로 출력		
 			
 			
 			String line="";
-			while((line=br.readLine())!=null) {
-				System.out.println(line);
+			while((line=br.readLine())!=null) {//버퍼에서 읽은 파일 줄 단위로 읽기... 더이상 읽을게 없으면 null 반환
+				System.out.println(line);//콘솔이 아닌 지정한 파일로 출력됨.
 			}
 			System.err.println("성공");
 			pw.close();
